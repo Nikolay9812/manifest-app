227516d2a428311520d8400a8e1400a9a8e37ddc
Nikolay9812 <73443601+Nikolay9812@users.noreply.github.com>
2024-06-06 20:07:46 +0200

    new3

--- api/index.js
+++ api/index.js
@@ -13,47 +13,55 @@ import path from 'path';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => { console.log('Database is connected!'); })
-    .catch(err => { console.log(err); });
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO)
+    .then(() => { 
+        console.log('Database is connected!'); 
+    })
+    .catch(err => { 
+        console.error('Failed to connect to the database:', err); 
+    });
 
 const __dirname = path.resolve();
 const app = express();
 
+// CORS Configuration
 app.use(cors({
-    origin: 'https://manifest-app.onrender.com/', // Adjust this to your frontend's domain
+    origin: 'https://manifest-app.onrender.com', // Adjust this to your frontend's domain
     credentials: true,
-  }));
+}));
 
 app.use(express.json());
 app.use(cookieParser());
 
+// Route Definitions
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/manifest', manifestRoutes);
-app.use('/api/stantion', stationRoutes);
+app.use('/api/stantion', stationRoutes); // Note: Possible typo; should it be '/api/station'?
 app.use('/api/plate', plateRoutes);
 app.use('/api/tor', torRoutes);
 
+// Serve static files from client/dist
 app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
+// Catch-all route to handle client-side routing
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
+// Error Handling Middleware
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
     res.status(statusCode).json({
         success: false,
         statusCode,
-        message
+        message,
     });
 });
 
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
