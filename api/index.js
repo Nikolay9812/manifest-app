81013d320cc752b07337a77869b88866786a785c
Nikolay9812 <73443601+Nikolay9812@users.noreply.github.com>
2024-06-06 04:38:39 +0200

    new

--- api/index.js
+++ api/index.js
@@ -1,51 +1,54 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js'
-import manifestRoutes from './routes/manifest.route.js'
-import stationRoutes from './routes/station.route.js'
-import plateRoutes from './routes/plate.route.js'
-import torRoutes from './routes/tor.route.js'
-import cookieParser from 'cookie-parser'
-import path from 'path'
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import manifestRoutes from './routes/manifest.route.js';
+import stationRoutes from './routes/station.route.js';
+import plateRoutes from './routes/plate.route.js';
+import torRoutes from './routes/tor.route.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 
-dotenv.config()
+dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-    .then(() => { console.log('Database is connected!') })
-    .catch(err => { console.log(err) })
-
-    const __dirname = path.resolve()
-
-const app = express()
-
-app.use(express.json()) //This will allow us to use json in POSTMAN!!!
-app.use(cookieParser())
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+mongoose.connect(process.env.MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
+    .then(() => { console.log('Database is connected!'); })
+    .catch(err => { console.log(err); });
 
-app.use('/api/user', userRoutes)
-app.use('/api/auth', authRoutes)
-app.use('/api/manifest', manifestRoutes)
-app.use('/api/stantion', stationRoutes)
-app.use('/api/plate', plateRoutes)
-app.use('/api/tor', torRoutes)
+const __dirname = path.resolve();
+const app = express();
 
-app.use(express.static(path.join(__dirname, '/client/dist')))
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/manifest', manifestRoutes);
+app.use('/api/stantion', stationRoutes);
+app.use('/api/plate', plateRoutes);
+app.use('/api/tor', torRoutes);
+
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'client','dist','index.html'))
-})
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500
-    const message = err.message || 'Iternal Server Error'
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
     res.status(statusCode).json({
         success: false,
         statusCode,
         message
-    })
-})
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
