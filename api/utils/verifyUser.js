2448506e2162c723e0870a27695d2e2f05131dc7
Nikolay9812 <73443601+Nikolay9812@users.noreply.github.com>
2024-06-09 08:32:03 +0200

    new6

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,23 +1,17 @@
-import jwt from 'jsonwebtoken';
-import { errorHandler } from './error.js';
+import jwt from 'jsonwebtoken'
+import { errorHandler } from './error.js'
 
-export const verifyToken = (req, res, next) => {
-    const token = req.cookies.access_token;
+export const verifyToken = (req, res, next) => { 
+    const token = req.cookies.access_token
     
-    console.log('Received Token:', token);
-
     if (!token) {
-        console.log('No token provided');
-        return next(errorHandler(401, 'Unauthorized'));
+        return next(errorHandler(401,'Unauthorized'))
     }
-
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) {
-            console.log('Token verification failed:', err);
-            return next(errorHandler(401, 'Unauthorized'));
+            return next(errorHandler(401,'Unauthorized'))
         }
-        console.log('Token verified, user:', user);
-        req.user = user;
-        next();
-    });
-};
+        req.user = user
+        next()
+    })
+}
